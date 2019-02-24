0e2a9274ba7048c789e4fcc7898d80ae3d713319
jahid <32840051+polashbabu@users.noreply.github.com>
2019-02-24 15:51:56 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace input_techniqe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number ");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your third number ");
+            int number3 = int.Parse(Console.ReadLine());
+            int add=number1 + number2 + number3;
+            Console.WriteLine("The sum is\n" + add);
+            Console.ReadKey();
+
+        }
+    }
+}
